1944b6b7be370e88baa36fc3840398d9d71790be
Matt Dancho <mdancho@gmail.com>
2021-07-07 10:03:26 -0400

    pre-reorganization

--- app.py
+++ app.py
@@ -1,58 +1,30 @@
-# import dash
-# import dash_core_components as dcc
-# import dash_html_components as html
-# from dash.dependencies import Input, Output, ClientsideFunction
+# LL PRO BONUS: R SHINY APPLICATION ----
+# BUSINESS SCIENCE LEARNING LABS ----
+# LAB 59: CUSTOMER LIFETIME VALUE | PYTHON DASH ----
+# ----
 
-# import numpy as np
-# import pandas as pd
-# import datetime
-# from datetime import datetime as dt
-# import pathlib
-
-# app = dash.Dash(
-#     __name__,
-#     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
-# )
-# app.title = "Clinical Analytics Dashboard"
-
-# server = app.server
-# app.config.suppress_callback_exceptions = True
-
-# # PATHS
-# BASE_PATH = pathlib.Path(__file__).parent.resolve()
-# DATA_PATH = BASE_PATH.joinpath("data").resolve()
-# ART_PATH = BASE_PATH.joinpath("artifacts").resolve()
-
-# # DATA
-# predictions_df = pd.read_pickle(ART_PATH.joinpath("predictions_df.pkl"))
-
-# -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+# LIBRARIES
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+
 import plotly.express as px
 
 import pandas as pd
 import pathlib
 
 # APP SETUP
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # PATHS
-
 BASE_PATH = pathlib.Path(__file__).parent.resolve()
 ART_PATH = BASE_PATH.joinpath("artifacts").resolve()
 
 # DATA
-
 predictions_df = pd.read_pickle(ART_PATH.joinpath("predictions_df.pkl"))
 
 df = predictions_df \
@@ -61,7 +33,6 @@ df = predictions_df \
     )
 
 # LAYOUT
-
 app.layout = html.Div([
     dcc.Graph(id='graph-slider'),
     dcc.Slider(
@@ -88,8 +59,8 @@ def update_figure(spend_delta_max):
         color_continuous_midpoint=0, 
         opacity=0.5, 
         color_continuous_scale='IceFire', 
-        # trendline='lowess', 
-        # trendline_color_override='black'
+        hover_name='customer_id',
+        hover_data=['spend_90_total', 'pred_spend']
     ) \
         .update_layout(
             {
